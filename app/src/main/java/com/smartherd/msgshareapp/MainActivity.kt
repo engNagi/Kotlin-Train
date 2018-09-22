96ba39cd8cb5a67daf4a9e28f3d0ad2ce4eef1d1
engNagi <eng.m.nagi@gmail.com>
2018-09-22 17:11:04 +0200

    section 3 video 3

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -19,12 +19,32 @@ import kotlinx.android.synthetic.main.activity_main.*
             Toast.makeText(this, "Button Was Clicked", Toast.LENGTH_SHORT).show()
         }
 
+        //creating a listener event for btnSendMsgToNextActivity
         btnSendMsgToNextActivity.setOnClickListener {
+            //getting the text from etUserMsg textbox and save it to text value.
             val text : String = etUserMsg.text.toString()
+            //send the text to Toast.makeText to show the text entered by the user.
             Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 
+            //explicit intent where we know which activity our intent would use.
             val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra ("user_message", text)
+            //starting our intent.
             startActivity(intent)
+
+
+        }
+
+        btnShareTOOtherApps.setOnClickListener{
+            val text : String = etUserMsg.text.toString()
+            //implicit intent there is no activity defined as we do not know which activity we are going to use.
+            val intent = Intent()
+            //starting our intent.
+            intent.action = Intent.ACTION_SEND
+
+            intent.putExtra(Intent.EXTRA_TEXT, text)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to"))
 
 
         }
--- app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
@@ -2,10 +2,20 @@ package com.smartherd.msgshareapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        // get the intent from the main activity and assign it to bundle nullable
+        val bundle: Bundle? = intent.extras
+        //use the bundle to extract the text from the intent of the MainActivity
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        txtViewUSerMsg.text = msg
     }
 }
